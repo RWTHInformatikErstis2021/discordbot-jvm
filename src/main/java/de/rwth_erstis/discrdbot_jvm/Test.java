44daf8b7b510af3afe651fb188d8908bb9cfc28b
Dominik Lenz <dominikalexanderlenz@gmail.com>
2021-09-09 22:13:06 +0200

    Fix packaging and java interop

---
+++ src/main/java/de/rwth_erstis/discrdbot_jvm/Test.java
@@ -0,0 +1,7 @@
+package de.rwth_erstis.discrdbot_jvm;
+
+public class Test {
+    public int test(){
+        return -1;
+    }
+}
--- src/main/kotlin/de/rwth_erstis/discrdbot_jvm/Launcher.kt
+++ src/main/kotlin/de/rwth_erstis/discrdbot_jvm/Launcher.kt
@@ -1,11 +1,13 @@
 package de.rwth_erstis.discrdbot_jvm
 
+import de.rwth_erstis.discrdbot_jvm.Test
 import io.github.cdimascio.dotenv.dotenv
 import kotlin.system.exitProcess
 import de.rwth_erstis.discrdbot_jvm.core.BotImpl as Bot
 
 
 fun main(args: Array<String>) {
+    print(Test().test())
     val env = dotenv()
     val token = env["TOKEN"] ?: exitProcess(1)
     val bot = Bot(token)
